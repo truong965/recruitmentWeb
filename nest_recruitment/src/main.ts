e33496c715f868aa222a2d77ca09d3e2a75759af
truong965 <22714791.truong@student.iuh.edu.vn>
2026-01-13 19:43:10 +0700

    config version

--- nest_recruitment/src/main.ts
+++ nest_recruitment/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transform.interceptor';
@@ -23,6 +23,12 @@ async function bootstrap() {
     origin: 'http://localhost:3000/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
+  });
+  //config version
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI, //v
+    defaultVersion: ['1'], //v1
   });
   await app.listen(configService.get<string>('PORT') ?? 8000);
 }
